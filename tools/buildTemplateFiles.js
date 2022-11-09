4ee406aab22212c37b08d23f207efc565299a989
Andy Fillebrown <andy.fillebrown@gmail.com>
2022-11-09 20:04:56 +0000

    Add CLI options and usage to buildTemplateFiles.js

--- tools/buildTemplateFiles.js
+++ tools/buildTemplateFiles.js
@@ -1,6 +1,59 @@
 const fs = require("fs");
 const path = require("path");
+const CliArgs = require("command-line-args");
+const CliUsage = require("command-line-usage");
 const Handlebars = require("handlebars/dist/handlebars.min.js");
+
+const cliOptions = [
+    {
+        name: "build-dir",
+        type: String,
+        description: "Should be the same as CMAKE_BINARY_DIR."
+    },
+    {
+        name: "helper-files",
+        type: String,
+        multiple: true,
+        description: "The list of Handlebar helper files to require and instantiate."
+    },
+    {
+        name: "json-files",
+        type: String,
+        multiple: true,
+        description: "The list of JSON files used to expand the template source."
+    },
+    {
+        name: "source",
+        type: String,
+        description: "The template source file to build."
+    },
+    {
+        name: "verbose",
+        alias: "v",
+        type: Boolean,
+        description: "Turn on debug output."
+    },
+    {
+        name: "help",
+        alias: "h",
+        type: Boolean,
+        description: "Print usage guide."
+    }
+];
+
+const arg = CliArgs(cliOptions, { camelCase: true});
+
+if (arg.help) {
+    const cliUsage = [
+        {
+            header: "Options",
+            optionList: cliOptions
+        }
+    ]
+
+    console.log(CliUsage(cliUsage));
+    return 0;
+}
 
 const isVerbose = process.argv.indexOf("--verbose") != -1 || process.argv.indexOf("-v") != -1;
 if (isVerbose) {
@@ -13,26 +66,17 @@ if (isVerbose) {
 // - All other path arguments are expected to be relative to the root directory.
 // - The `helperFilename` arg is expected to be given as the Typescript source filename.
 
-let argStartIndex = 2;
-const buildDir = process.argv[argStartIndex++];
-const helperFilename = process.argv[argStartIndex++];
-const jsonFilename = process.argv[argStartIndex++];
-const sourceFilename = process.argv[argStartIndex++];
-
 if (isVerbose) {
-    console.log("\nArguments:\n")
-    console.log(`buildDir = ${buildDir}`);
-    console.log(`helper = ${helperFilename}`);
-    console.log(`json = ${jsonFilename}`);
-    console.log(`source = ${sourceFilename}`);
+    console.log(`\nArguments:`)
+    console.log(arg);
 }
 
 
 // Load helper
 
-const buildRootDir = path.join(buildDir, ".root");
-const helperTypescriptBasename = path.basename(helperFilename);
-const helperRelativeDir = path.dirname(helperFilename);
+const buildRootDir = path.join(arg.buildDir, ".root");
+const helperTypescriptBasename = path.basename(arg.helperFiles[0]);
+const helperRelativeDir = path.dirname(arg.helperFiles[0]);
 const helperJavascriptBasename = helperTypescriptBasename.replace(".ts", ".js");
 const helperBuildFilename = path.join(buildRootDir, helperRelativeDir, helperJavascriptBasename);
 const helperBasenameWithoutExtension = helperJavascriptBasename.replace(".js", "");
@@ -41,13 +85,14 @@ const helperClassName =
 ;
 
 if (isVerbose) {
-    console.log("\nVariables:\n");
-    console.log(`buildRootDir = ${buildRootDir}`);
-    console.log(`helperTypescriptBasename = ${helperTypescriptBasename}`);
-    console.log(`helperRelativeDir = ${helperRelativeDir}`);
-    console.log(`helperJavascriptBasename = ${helperJavascriptBasename}`);
-    console.log(`helperBuildFilename = ${helperBuildFilename}`);
-    console.log(`helperClassName = ${helperClassName}`);
+    console.log(`\nConstants:`);
+    console.log(`{`);
+    console.log(`  buildRootDir = ${buildRootDir}`);
+    console.log(`  helperTypescriptBasename = ${helperTypescriptBasename}`);
+    console.log(`  helperRelativeDir = ${helperRelativeDir}`);
+    console.log(`  helperJavascriptBasename = ${helperJavascriptBasename}`);
+    console.log(`  helperBuildFilename = ${helperBuildFilename}`);
+    console.log(`  helperClassName = ${helperClassName}`);
 }
 
 require(helperBuildFilename);
@@ -56,25 +101,28 @@ eval(`new ${helperClassName}(Handlebars)`);
 
 // Build template source
 
-const sourceBuildDir = path.join(buildRootDir, path.dirname(sourceFilename));
-const sourceBasename = path.basename(sourceFilename);
+const sourceBuildDir = path.join(buildRootDir, path.dirname(arg.source));
+const sourceBasename = path.basename(arg.source);
 const sourceBuildFilename = path.join(sourceBuildDir, sourceBasename);
 
 if (isVerbose) {
-    console.log(`sourceBuildDir = ${sourceBuildDir}`);
-    console.log(`sourceBasename = ${sourceBasename}`);
-    console.log(`sourceBuildFilename = ${sourceBuildFilename}`);
+    console.log(`  sourceBuildDir = ${sourceBuildDir}`);
+    console.log(`  sourceBasename = ${sourceBasename}`);
+    console.log(`  sourceBuildFilename = ${sourceBuildFilename}`);
+    console.log(`}`);
 }
 
-const source = fs.readFileSync(sourceFilename, 'ascii');
+const source = fs.readFileSync(arg.source, 'ascii');
 const template = Handlebars.compile(source);
 
-const jsonString = fs.readFileSync(jsonFilename, 'ascii');
+const jsonString = fs.readFileSync(arg.jsonFiles[0], 'ascii');
 const output = template(JSON.parse(jsonString));
 
 if (isVerbose) {
-    console.log("\nOutput:\n")
-    console.log(output);
+    console.log(`\nOutput:`);
+    console.log(`{`);
+    console.log(`  ${output.replaceAll("\n", "\n  ")}`);
+    console.log(`}`);
 }
 
 fs.mkdirSync(sourceBuildDir, { recursive: true });
@@ -82,5 +130,5 @@ fs.writeFileSync(sourceBuildFilename, output, 'ascii');
 
 
 if (isVerbose) {
-    console.log("\nbuildTemplateFiles.js - done\n");
+    console.log(`\nbuildTemplateFiles.js - done\n`);
 }
