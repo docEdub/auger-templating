4c9d00cfc5fe7aec32c5fa1940a625fec82da940
Andy Fillebrown <andy@docEdub.com>
2022-12-05 07:01:55 -0500

    Remove unused arg in cabbageTemplateHelper.ts

--- src/Cabbage/cabbageTemplateHelper.ts
+++ src/Cabbage/cabbageTemplateHelper.ts
@@ -5,7 +5,7 @@ import { Form } from "./Types/form";
 // - The class name must be the same as the filename starting with an uppercase letter, without the `.ts` suffix.
 global.CabbageTemplateHelper = class {
     constructor(Handlebars: any) {
-        Handlebars.registerHelper("Cabbage", (input, options) => {
+        Handlebars.registerHelper("Cabbage", (input) => {
             let output = "";
             output += `<Cabbage>\n`;
             output += (new Form(input.data.root.form)).output();
