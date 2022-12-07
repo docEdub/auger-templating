ff8947de2f376d1216589851327caa2d25137e67
Andy Fillebrown <andy@docEdub.com>
2022-12-07 12:59:29 -0500

    Get all 5 Csound log level templates working

--- csd/test-a.csd
+++ csd/test-a.csd
@@ -7,7 +7,11 @@
 {{csound_macros}}
 
 instr A
-    {{log_i_trace}}("%s %d, %d, %d ...", "test", 1, 2, 3)
+    {{log_i_trace}}("%s %d, %d, %d ...", "test trace", 1, 2, 3)
+    {{log_i_debug}}("%s %d, %d, %d ...", "test debug", 1, 2, 3)
+    {{log_i_info}}("%s %d, %d, %d ...", "test info", 1, 2, 3)
+    {{log_i_warning}}("%s %d, %d, %d ...", "test warning", 1, 2, 3)
+    {{log_i_error}}("%s %d, %d, %d ...", "test error", 1, 2, 3)
 endin
 
 </CsInstruments>
--- src/Csound/csoundTemplateHelper.ts
+++ src/Csound/csoundTemplateHelper.ts
@@ -3,28 +3,37 @@
 // - The class name must be the same as the filename starting with an uppercase letter, without the `.ts` suffix.
 global.CsoundTemplateHelper = class {
     constructor(Handlebars: any) {
+        const logLevels = [
+            { name: `trace`, abbreviation: `trc` },
+            { name: `debug`, abbreviation: `dbg` },
+            { name: `info`, abbreviation: `inf` },
+            { name: `warning`, abbreviation: `wrn` },
+            { name: `error`, abbreviation: `err` }
+        ]
+
         Handlebars.registerHelper(`csound_macros`, (input) => {
             let output = ``;
-            if (input.data.root.log_i_trace_enabled) {
-                output += `#define log_i_trace(args) #\n`;
-                output += `    prints($args)\n`;
-                output += `    prints("\\n")\n`;
-                output += `#\n`;
+            for (let i = 0; i < logLevels.length; i++) {
+                const logLevel = logLevels[i];
+                if (input.data.root[`log_i_${logLevel.name}_enabled`]) {
+                    output += `#define log_i_${logLevel.name}(args) #\n`;
+                    output += `    prints($args)\n`;
+                    output += `    prints("\\n")\n`;
+                    output += `#\n`;
+                }
             }
-            output += `# define noop(a) ##`
             return new Handlebars.SafeString(output);
         });
 
-        Handlebars.registerHelper(`log_i_trace`, (input) => {
-            let output = ``;
-            if (input.data.root.log_i_trace_enabled) {
-                output += `prints("[trc] ${input.data.root.fileName}(${input.loc.start.line}): ")\n`;
-                output += `    $log_i_trace`;
-            }
-            else {
-                output += `$noop`;
-            }
-            return new Handlebars.SafeString(output);
-        })
+        for (let i = 0; i < logLevels.length; i++) {
+            const logLevel = logLevels[i];
+            Handlebars.registerHelper(`log_i_${logLevel.name}`, (input) => {
+                const disabler = input.data.root[`log_i_${logLevel.name}_enabled`] ? `` : `; `;
+                let output = ``;
+                output += `${disabler}prints("[${logLevel.abbreviation}] ${input.data.root.fileName}(${input.loc.start.line}): ")\n`;
+                output += `    ${disabler}$log_i_${logLevel.name}`;
+                return new Handlebars.SafeString(output);
+            })
+        }
     }
 }
