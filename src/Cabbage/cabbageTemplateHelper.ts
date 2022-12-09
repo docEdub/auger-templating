8e4a546e64766df21b4bc18101981536fd161a7e
Andy Fillebrown <andy@docEdub.com>
2022-12-09 18:38:57 -0500

    Improve comment

--- src/Cabbage/cabbageTemplateHelper.ts
+++ src/Cabbage/cabbageTemplateHelper.ts
@@ -3,7 +3,7 @@ import { Form } from './Types/form';
 import { Widget } from './Types/widget';
 
 // Note:
-// - The class name must be the same as the filename starting with an uppercase letter, without the `.ts` suffix.
+// - The class name must be the same as the filename, starting with an uppercase letter and without the `.ts` suffix.
 global.CabbageTemplateHelper = class {
     constructor(Handlebars: any) {
         Handlebars.registerHelper("Cabbage", (input) => {
