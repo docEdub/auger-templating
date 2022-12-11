69499bb82b23f15c5abaca35e521ec76a8a1a322
Andy Fillebrown <andy@docEdub.com>
2022-12-10 22:21:09 -0500

    Add Widget.margin property

--- src/Cabbage/Types/widget.ts
+++ src/Cabbage/Types/widget.ts
@@ -2,6 +2,8 @@
 import { Box } from './box';
 import { Group } from './group';
 
+import { cssMatte, CssMatte } from '../../Utility/cssMatte';
+
 export class Widget extends Box {
     constructor(json: any) {
         super(json);
@@ -18,6 +20,16 @@ export class Widget extends Box {
     public get children() {
         return this._children as Array<Widget>;
     }
+
+    public get margin() {
+        return this._margin;
+    }
+
+    public set margin(value: cssMatte) {
+        this._margin = CssMatte.AsNumberArray(value, this.width, this.height);
+    }
+
+    private _margin = new Array<number>(4);
 }
 
 if (global.testing) {
