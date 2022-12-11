d2878e054ea8d96b02de28d782142477c0abc7a3
Andy Fillebrown <andy@docEdub.com>
2022-12-10 22:19:33 -0500

    Add string type to cssMatte union to enable setting it from a css string

--- src/Utility/cssMatte.ts
+++ src/Utility/cssMatte.ts
@@ -2,40 +2,52 @@
 import { Css } from './css';
 import { cssNumber, CssNumber } from './cssNumber';
 
-export type cssMatte = Array<cssNumber>;
+export type cssMatte = Array<cssNumber> | string;
 
 export class CssMatte {
     public static AsNumberArray(cssMatte: cssMatte, referenceWidth?: number, referenceHeight?: number) {
-        const array = new Array<number>(4);
-        array.fill(0);
+        const outputArray = new Array<number>(4);
+        outputArray.fill(0);
         if (!cssMatte) {
-            return array;
+            return outputArray;
         }
-        if (cssMatte.length === 0) {
-            return array;
+        let inputArray: Array<cssNumber> = null;
+        if (Array.isArray(cssMatte)) {
+            inputArray = cssMatte;
         }
-        array[Css.Top] = CssNumber.AsNumber(cssMatte[Css.Top], referenceHeight);
-        if (cssMatte.length === 1) {
-            array[Css.Left] = CssNumber.AsNumber(cssMatte[Css.Top], referenceWidth);
-            array[Css.Bottom] = array[Css.Top];
-            array[Css.Right] = array[Css.Left];
+        else if (typeof cssMatte === `string`) {
+            const inputString = cssMatte as string;
+            const inputStringArray = inputString.split(` `);
+            inputArray = new Array<cssNumber>();
+            for (let i = 0; i < Math.min(4, inputStringArray.length); i++) {
+                inputArray[i] = inputStringArray[i];
+            }
         }
-        else if (cssMatte.length === 2) {
-            array[Css.Left] = CssNumber.AsNumber(cssMatte[Css.Left], referenceWidth);
-            array[Css.Bottom] = array[Css.Top];
-            array[Css.Right] = array[Css.Left];
+        if (inputArray.length === 0) {
+            return outputArray;
         }
-        else if (cssMatte.length === 3) {
-            array[Css.Left] = CssNumber.AsNumber(cssMatte[Css.Left], referenceWidth);
-            array[Css.Bottom] = CssNumber.AsNumber(cssMatte[Css.Bottom], referenceHeight);
-            array[Css.Right] = array[Css.Left];
+        outputArray[Css.Top] = CssNumber.AsNumber(inputArray[Css.Top], referenceHeight);
+        if (inputArray.length === 1) {
+            outputArray[Css.Left] = CssNumber.AsNumber(inputArray[Css.Top], referenceWidth);
+            outputArray[Css.Bottom] = outputArray[Css.Top];
+            outputArray[Css.Right] = outputArray[Css.Left];
+        }
+        else if (inputArray.length === 2) {
+            outputArray[Css.Left] = CssNumber.AsNumber(inputArray[Css.Left], referenceWidth);
+            outputArray[Css.Bottom] = outputArray[Css.Top];
+            outputArray[Css.Right] = outputArray[Css.Left];
+        }
+        else if (inputArray.length === 3) {
+            outputArray[Css.Left] = CssNumber.AsNumber(inputArray[Css.Left], referenceWidth);
+            outputArray[Css.Bottom] = CssNumber.AsNumber(inputArray[Css.Bottom], referenceHeight);
+            outputArray[Css.Right] = outputArray[Css.Left];
         }
         else {
-            array[Css.Left] = CssNumber.AsNumber(cssMatte[Css.Left], referenceWidth);
-            array[Css.Bottom] = CssNumber.AsNumber(cssMatte[Css.Bottom], referenceHeight);
-            array[Css.Right] = CssNumber.AsNumber(cssMatte[Css.Right], referenceWidth);
+            outputArray[Css.Left] = CssNumber.AsNumber(inputArray[Css.Left], referenceWidth);
+            outputArray[Css.Bottom] = CssNumber.AsNumber(inputArray[Css.Bottom], referenceHeight);
+            outputArray[Css.Right] = CssNumber.AsNumber(inputArray[Css.Right], referenceWidth);
         }
-        return array;
+        return outputArray;
     }
 }
 
--- tests/Utility/cssMatte.test.js
+++ tests/Utility/cssMatte.test.js
@@ -24,5 +24,17 @@ describe(`CssMatte`, () => {
         test(`returns [1, 2, 3, 4] when given [1, 2, 3, 4, 5]`, () => {
             expect(CssMatte.AsNumberArray([1, 2, 3, 4, 5])).toStrictEqual([1, 2, 3, 4]);
         });
+        test(`returns [1, 1, 1, 1] when given "1"`, () => {
+            expect(CssMatte.AsNumberArray(`1`)).toStrictEqual([1, 1, 1, 1]);
+        });
+        test(`returns [1, 2, 1, 2] when given "1 2"`, () => {
+            expect(CssMatte.AsNumberArray(`1 2`)).toStrictEqual([1, 2, 1, 2]);
+        });
+        test(`returns [1, 2, 3, 2] when given "1 2 3"`, () => {
+            expect(CssMatte.AsNumberArray(`1 2 3`)).toStrictEqual([1, 2, 3, 2]);
+        });
+        test(`returns [1, 2, 3, 4] when given "1 2 3 4"`, () => {
+            expect(CssMatte.AsNumberArray("1 2 3 4")).toStrictEqual([1, 2, 3, 4]);
+        });
     });
 });
