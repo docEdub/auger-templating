358ec8a40d2d219c9864575fa8fc85063d9b6157
Andy Fillebrown <andy@docEdub.com>
2022-12-16 08:26:02 -0500

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         ["src/Csound/csoundTemplateHelper"]: path.resolve(__dirname, "src/Csound/csoundTemplateHelper.ts"),
     },
     output: {
-        // clean: true, // NB: Cleaning .build/.root deletes the output .csd, which disrupts Cabbage's hot reload.
+        // clean: true, // NB: Cleaning .build/.root deletes the output .csd, which disrupts Cabbage's live reload.
         filename: '[name].js',
         path: path.join(__dirname, '.build/.root'),
         publicPath: '/',
