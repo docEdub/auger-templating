1fa51ac359c6398e492250d64ddbfd0e13efc6d3
Andy Fillebrown <andy@docEdub.com>
2022-12-18 00:02:10 -0500

    Make CsoundTemplateHelpers folder easier to relocate

--- CsoundTemplateHelpers/webpack.config.js
+++ CsoundTemplateHelpers/webpack.config.js
@@ -1,42 +1,45 @@
 const path = require("path");
 const WebpackTouch = require('webpack-touch');
 
-const CsoundTemplateHelpers_BuildOutputDir = path.join(__dirname, '..', '.build', '.output', 'CsoundTemplateHelpers')
+module.exports = (env) => {
+    const subdir = __dirname.replace(`${env.rootDir}/`, ``);
+    const CsoundTemplateHelpers_BuildOutputDir = path.join(env.rootDir, '.build', '.output', subdir);
 
-module.exports = {
-    entry: {
-        ["cabbageTemplateHelper"]: path.resolve(__dirname, "src/cabbageTemplateHelper.ts"),
-        ["csoundTemplateHelper"]: path.resolve(__dirname, "src/csoundTemplateHelper.ts"),
-    },
-    output: {
-        // clean: true, // NB: Cleaning .build/.output deletes the output .csd, which disrupts Cabbage's live reload.
-        filename: '[name].js',
-        path: path.join(CsoundTemplateHelpers_BuildOutputDir, 'src'),
-        publicPath: '/',
-    },
-    plugins: [
-        new WebpackTouch({ filename: path.join(CsoundTemplateHelpers_BuildOutputDir, '.webpack.stamp'), delay: 0 })
-    ],
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.js$/,
-                enforce: "pre",
-                use: ["source-map-loader"],
-            }
-        ]
-    },
-    mode: "development",
+    return {
+        entry: {
+            ["cabbageTemplateHelper"]: path.resolve(__dirname, "src/cabbageTemplateHelper.ts"),
+            ["csoundTemplateHelper"]: path.resolve(__dirname, "src/csoundTemplateHelper.ts"),
+        },
+        output: {
+            // clean: true, // NB: Cleaning .build/.output deletes the output .csd, which disrupts Cabbage's live reload.
+            filename: '[name].js',
+            path: path.join(CsoundTemplateHelpers_BuildOutputDir, 'src'),
+            publicPath: '/',
+        },
+        plugins: [
+            new WebpackTouch({ filename: path.join(CsoundTemplateHelpers_BuildOutputDir, '.webpack.stamp'), delay: 0 })
+        ],
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+        devtool: "inline-source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.js$/,
+                    enforce: "pre",
+                    use: ["source-map-loader"],
+                }
+            ]
+        },
+        mode: "development",
 
-    // Allow node modules to be used in TypeScript, e.g. `const fs = require(fs)`.
-    target: "node",
+        // Allow node modules to be used in TypeScript, e.g. `const fs = require(fs)`.
+        target: "node",
+    }
 };
